b47a4638bbb671f8889ff2e1c90615a216bfb982
im-amarthya <amarthya.keerthana@gmail.com>
2024-02-01 17:03:28 +0530

    Added text

--- App1.java
+++ App1.java
@@ -2,6 +2,6 @@ class App1
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.");
+        System.out.println("My First Git Global Program.");
     }
 };

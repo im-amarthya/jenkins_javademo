0440d58a2bc34a30a7670df2445fd7cb0aedce12
im-amarthya <amarthya.keerthana@gmail.com>
2024-02-01 12:22:53 +0530

    Edited Push

--- App1.java
+++ App1.java
@@ -1,5 +1,5 @@
 public class App1{
     public static void main(Strings[] args){
-        System.out.println("Java App Pulled and Build by Jenkins")
+        System.out.println("Java App Pulled and Build by Jenkins");
     }
 }
